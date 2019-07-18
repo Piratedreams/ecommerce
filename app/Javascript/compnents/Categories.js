f985ac51b53219323a62037997684111d0eb3d7c
Ben Wall <wall.benjamina@gmail.com>
2019-07-18 13:39:54 -0600

    listing out categories and building react infrastructure

--- app/Javascript/compnents/Categories.js
+++ app/Javascript/compnents/Categories.js
@@ -4,11 +4,27 @@ import React from 'react'
 class Categories extends React.Component {
     render () {
 
+        const getCategories = async = () => {
+            try {
+
+
+                
+
+            } catch (err) {
+            console.log(err);
+            return err;
+            }
+        }
+
+
         return (
         <body>
             <h1>This is the Categories page.</h1>
             <div>
-
+                <ul>
+                    <li> {category.title} </li>
+                </ul>
+                
             </div>
         </body>
         )
